ae2727be05cff78e5fdea48f3ee012b9c142919f
shungo0222 <kimura.shungo@gmail.com>
2020-08-04 23:35:40 +0900

    Created ABC109

---
+++ ABC/ABC109/a.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int a, b;
+    cin >> a >> b;
+    if (a != 2 && b != 2) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
