1685bfbb4bbd05518b02434d5a09c33b2deefa92
shungo0222 <kimura.shungo@gmail.com>
2020-08-06 21:07:35 +0900

    Created sumitrust2019

---
+++ Other/sumitrust2019/a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int m1, m2, d1, d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+    if (d2 == 1) cout << 1 << endl;
+    else cout << 0 << endl;
+    return 0;
+}
