e56a955b72aa39a7cc8769a97b8ea18e389b4058
shungo0222 <kimura.shungo@gmail.com>
2020-08-13 11:45:03 +0900

    Solved ABC100 A

---
+++ ABC/ABC100/a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int a, b;
+    cin >> a >> b;
+    if (a <= 8 && b <= 8) cout << "Yay!" << endl;
+    else cout << ":(" << endl;
+    return 0;
+}
