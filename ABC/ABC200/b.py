63b37ccf923df69a6df0a23ade81a23308a38dd1
shungo0222 <kimura.shungo@gmail.com>
2021-05-08 23:18:17 +0900

    Created ABC200

---
+++ ABC/ABC200/b.py
@@ -0,0 +1,13 @@
+def main():
+    n, k = map(int, input().split())
+    
+    for _ in range(k):
+        if n % 200 == 0:
+            n //= 200
+        else:
+            n = n * 1000 + 200
+    
+    print(n)
+
+if __name__ == '__main__':
+    main()
