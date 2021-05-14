cdddec0a4cd9d7f8a1da73428538526398f5f35f
shungo0222 <kimura.shungo@gmail.com>
2021-05-14 22:29:50 +0900

    Created ABC077

---
+++ ABC/ABC077/a.py
@@ -0,0 +1,10 @@
+def main():
+    c = [list(input()) for _ in range(2)]
+    
+    if c[0][0] == c[1][2] and c[0][1] == c[1][1] and c[0][2] == c[1][0]:
+        print('YES')
+    else:
+        print('NO')
+
+if __name__ == '__main__':
+    main()
---
+++ ABC/ABC077/b.py
@@ -0,0 +1,8 @@
+import math
+
+def main():
+    n = int(input())
+    print(int(math.sqrt(n)) ** 2)
+
+if __name__ == '__main__':
+    main()
